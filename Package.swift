1ffe5f9f026fef05f1703a4fb9889a32c64bee24
Jean-Charles Mourey <jean-charles@mourey.com>
2025-05-17 00:01:35 +0200

    added more automation to simplify and automate package definition

--- Package.swift
+++ Package.swift
@@ -1,47 +1,60 @@
-// swift-tools-version: 6.1
-import PackageDescription
-
 let packageName = "TrivAIDependencies"
 
-let packages: [any PackageManifest] =
-    [
-        GithubPackageManifest.pointFreeDependencies,
-        GithubPackageManifest.eventSource,
-        LocalPackageManifest(name: "TrivAIModel", modules: ["TrivAIResponseModel"]),
-    ]
-
-let modules: [any ModuleManifest] =
-    [
-        DependencyModuleManifest(name: "TrivAIClient", dependencies: ["TrivAIResponseModel", "EventSource"], liveDependencies: ["EventSource"], resources: ["Resources"]),
-    ]
-
+let modules: [ModuleManifest] = [
+    DependencyModuleManifest(name: "TrivAIClient", dependencies: ["TrivAIResponseModel", "EventSource"], resources: ["Resources"], liveDependencies: ["EventSource"]),
+]
 
 // #include("Packages/PackageCommon/Package.swift")
 // #COMMON CODE
+// =================================================================================
 // WARNING: Only change COMMON CODE in included file, not in each Package.swift file as it will get overwritten
+// swift-tools-version: 6.1
+import PackageDescription
+
 let platforms: [SupportedPlatform] = [.iOS(.v18), .macOS(.v15), .tvOS(.v18)]
 
 // PACKAGES
-extension GithubPackageManifest {
-    static var pointFreeDependencies: Self {
-        .init(provider: "pointfreeco", name: "swift-dependencies", modules: ["Dependencies", "DependenciesMacros", "DependenciesTestSupport"], minVersion: "1.9.2")
-    }
-    static var eventSource: Self {
-        .init(provider: "Recouse", name: "EventSource", modules: ["EventSource"], minVersion: "0.1.4")
-    }
-    static var composableArchitecture: Self {
-        .init(provider: "pointfreeco", name: "swift-composable-architecture", modules: ["ComposableArchitecture"], minVersion: "1.19.1")
-    }
-}
+let githubPackages: [GithubPackageManifest] = [
+    .init(provider: "pointfreeco", name: "swift-dependencies", modules: ["Dependencies", "DependenciesMacros", "DependenciesTestSupport"], minVersion: "1.9.2"),
+    .init(provider: "Recouse", name: "EventSource", modules: ["EventSource"], minVersion: "0.1.4"),
+    .init(provider: "pointfreeco", name: "swift-composable-architecture", modules: ["ComposableArchitecture"], minVersion: "1.19.1"),
+    .init(provider: "swiftlang", name: "swift-syntax", modules: ["SwiftSyntaxMacros", "SwiftCompilerPlugin"], minVersion: "601.0.1"),
+    .init(provider: "pointfreeco", name: "swift-macro-testing", modules: ["MacroTesting"], minVersion: "0.6.2"),
+    .init(provider: "bitkey-oss", name: "sharing-firestore", modules: ["SharingFirestore"], minVersion: "0.2.0"),
+]
+
+let localPackages: [LocalPackageManifest] = [
+    .init(name: "TrivAIModel", modules: ["TrivAIResponseModel"]),
+    .init(name: "TrivAIMacros", modules: ["TrivAIMacros"]),
+    .init(name: "TrivAIDependencies", modules: ["TrivAIClient"]),
+]
+
+let availablePackages: [any PackageManifest] = githubPackages as [any PackageManifest] + localPackages as [any PackageManifest]
 
 // HELPERS
 let package = Package(
     name: packageName,
     platforms: platforms,
     products: modules.flatMap(\.products),
-    dependencies: packages.map(\.packageDependency),
-    targets: modules.flatMap(\.targets),
+    dependencies: modules.requiredGithubPackages + modules.requiredLocalPackages,
+    targets: modules.flatMap(\.targets)
 )
+
+extension [ModuleManifest] {
+    var allCombinedDependencies: Set<String> {
+        Set(flatMap(\.allCombinedDependencies))
+    }
+    
+    @MainActor
+    var requiredGithubPackages: [Package.Dependency] {
+        githubPackages.thatCanProvideSomeOf(allCombinedDependencies).map(\.packageDependency)
+    }
+
+    @MainActor
+    var requiredLocalPackages: [Package.Dependency] {
+        localPackages.thatCanProvideSomeOf(allCombinedDependencies).map(\.packageDependency)
+    }
+}
 
 // PACKAGE MANIFEST
 protocol PackageManifest {
@@ -50,14 +63,26 @@ protocol PackageManifest {
     var packageDependency: Package.Dependency { get }
 }
 
-@MainActor
-func targetDependency(_ moduleName: String) -> Target.Dependency {
-    // Look for the module in the packages
-    guard let package = packages.first(where: { $0.modules.contains(moduleName) })?.name else {
-        // if not found in the packages, assume the module is in the current package and just refer to it by name
-        return .init(stringLiteral: moduleName)
+extension String {
+    @MainActor
+    var targetDependency: Target.Dependency {
+        // Look for the module in the packages
+        guard let package = availablePackages.first(where: { $0.modules.contains(self) })?.name else {
+            // if not found in the packages, assume the module is in the current package and just refer to it by name
+            return .init(stringLiteral: self)
+        }
+        return .product(name: self, package: package)
     }
-    return .product(name: moduleName, package: package)
+    
+    var process: Resource {
+        Resource.process(self)
+    }
+}
+
+extension Array where Element: PackageManifest {
+    func thatCanProvideSomeOf(_ modules: Set<String>) -> [PackageManifest] {
+        filter { $0.modules.contains(where: modules.contains) }
+    }
 }
 
 struct GithubPackageManifest: PackageManifest {
@@ -81,28 +106,28 @@ struct LocalPackageManifest: PackageManifest {
 }
 
 // MODULE MANIFEST
-protocol ModuleManifest {
-    var name: String { get }
-    var dependencies: [String] { get }
-    var resources: [String] { get }
-    var products: [Product] { get }
-    @MainActor var targets: [Target] { get }
-}
-
-struct DependencyModuleManifest: ModuleManifest {
+class ModuleManifest {
     var name: String
     var dependencies: [String] = []
-    var liveDependencies: [String] = []
     var testDependencies: [String] = []
     var resources: [String] = []
     
-    var liveName: String { "\(name)Live" }
-
+    init(name: String, dependencies: [String] = [], testDependencies: [String] = [], resources: [String] = []) {
+        self.name = name
+        self.dependencies = dependencies
+        self.testDependencies = testDependencies
+        self.resources = resources
+    }
+    
+    var testName: String { "\(name)Tests" }
+    var allDependencies: [String] { dependencies }
+    var allTestDependencies: [String] { testDependencies + [name] }
+    var allCombinedDependencies: [String] { allDependencies + allTestDependencies }
+    
     var products: [Product] {
         [
-           .library(name: name, targets: [name]),
-           .library(name: liveName, targets: [liveName])
-       ]
+            .library(name: name, targets: [name])
+        ]
     }
     
     @MainActor
@@ -110,62 +135,94 @@ struct DependencyModuleManifest: ModuleManifest {
         [
             .target(
                 name: name,
-                dependencies: (dependencies + ["Dependencies", "DependenciesMacros"]).compactMap { targetDependency($0) },
-                resources: resources.map { .process($0) }
-            ),
-            .target(
-                name: liveName,
-                dependencies: (liveDependencies + ["Dependencies", name]).compactMap { targetDependency($0) }
+                dependencies: allDependencies.map(\.targetDependency),
+                resources: resources.map(\.process)
             ),
             .testTarget(
-                name: "\(name)Tests",
-                dependencies: (testDependencies + ["DependenciesTestSupport", name]).compactMap { targetDependency($0) }
+                name: testName,
+                dependencies: allTestDependencies.map(\.targetDependency)
+            ),
+        ]
+    }
+}
+
+class DependencyModuleManifest: ModuleManifest {
+    var liveDependencies: [String] = []
+        
+    init(name: String, dependencies: [String] = [], testDependencies: [String] = [], resources: [String] = [], liveDependencies: [String] = []) {
+        super.init(name: name, dependencies: dependencies, testDependencies: testDependencies, resources: resources)
+        self.liveDependencies = liveDependencies
+    }
+    
+    var liveName: String { "\(name)Live" }
+    var allLiveDependencies: [String] { liveDependencies + ["Dependencies", name] }
+
+    override var allDependencies: [String] { super.allDependencies + ["Dependencies", "DependenciesMacros"] }
+    override var allTestDependencies: [String] { super.allTestDependencies + ["DependenciesTestSupport"] }
+    override var allCombinedDependencies: [String] { super.allCombinedDependencies + liveDependencies }
+
+    override var products: [Product] {
+        super.products +
+        [
+           .library(name: liveName, targets: [liveName]),
+       ]
+    }
+    
+    @MainActor
+    override var targets: [Target] {
+        super.targets +
+        [
+            .target(
+                name: liveName,
+                dependencies: allLiveDependencies.map(\.targetDependency)
             )
         ]
     }
 }
 
-struct ModelModuleManifest: ModuleManifest {
-    var name: String
-    var dependencies: [String] = []
-    var resources: [String] = []
+class ModelModuleManifest: ModuleManifest {}
 
-    var products: [Product] {
-        [
-           .library(name: name, targets: [name]),
-       ]
-    }
-    
-    @MainActor
-    var targets: [Target] {
-        [
-            .target(
-                name: name,
-                dependencies: dependencies.compactMap { targetDependency($0) },
-                resources: resources.map { .process($0) }
-            )
-        ]
-    }
+class FeatureModuleManifest: ModuleManifest {
+    override var allDependencies: [String] { super.allDependencies + ["ComposableArchitecture"] }
 }
 
-struct FeatureModuleManifest: ModuleManifest {
-    var name: String
-    var dependencies: [String] = []
-    var resources: [String] = []
+// MACRO
+import CompilerPluginSupport    // needed for macro support
 
-    var products: [Product] {
+class MacroModuleManifest: ModuleManifest {
+    var macroClientName: String { "\(name)Client" }
+    var macroImplementationName: String { "\(name)Macros" }
+    
+    override var allDependencies: [String] { super.allDependencies + ["SwiftSyntaxMacros", "SwiftCompilerPlugin"] }
+    override var allTestDependencies: [String] { super.allTestDependencies + [macroImplementationName, "MacroTesting"] }
+    
+    override var products: [Product] {
+        super.products +
         [
-           .library(name: name, targets: [name]),
-       ]
+            .executable(name: macroClientName, targets: [macroClientName]),
+        ]
     }
     
     @MainActor
-    var targets: [Target] {
+    override var targets: [Target] {
         [
-            .target(
-                name: name,
-                dependencies: (dependencies + ["ComposableArchitecture"]).compactMap { targetDependency($0) }
-            )
+            // Macro implementation and its dependencies
+            .macro(
+                name: macroImplementationName,
+                dependencies: allDependencies.map(\.targetDependency)
+            ),
+            
+            // Library that exposes the macro and its features as part of its API which is used in client programs
+            .target(name: name, dependencies: [macroImplementationName].map(\.targetDependency)),
+            
+            // A (optional) client of the library, which is able to use the macro in its own code.
+            .executableTarget(name: macroClientName, dependencies: [name].map(\.targetDependency)),
+
+            // A test target used to test the macro implementation.
+            .testTarget(
+                name: testName,
+                dependencies: allTestDependencies.map(\.targetDependency)
+            ),
         ]
     }
 }
